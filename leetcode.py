df8a2c26dec2c89ba26dd94fc203c5aede2bb64f
NAMITHA R <nnnamitha26@gmail.com>
2025-10-23 18:12:16 +0530

    Implement containsNearbyDuplicate function
    Added a function to check for nearby duplicates in a list.

--- leetcode.py
+++ leetcode.py
@@ -1,4 +1,4 @@
-###3461-check-if-digits-are-equal-in-string-after-operations
+1###3461-check-if-digits-are-equal-in-string-after-operations
 class Solution(object):
     def hasSameDigits(self, s):
         """
@@ -10,4 +10,18 @@ class Solution(object):
             digits=[(digits[i] + digits[i+1])% 10 for i in range(len(digits)-1)]
         return digits[0] == digits[1]
 
+###
+2###
+nums =[1,2,3,1,2,3]
+k=2
+def containsNearbyDuplicate(nums, k):
+    hash={}
+    for i in nums:
+        hash[i]=hash.get(i,0)+1
+    for i,n in enumerate(nums):
+        if n in hash and i - hash[n] <= k:
+            return True
+        hash[n]=i
+    return False
+containsNearbyDuplicate(nums,k)
 ###
